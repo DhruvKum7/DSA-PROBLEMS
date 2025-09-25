d293cc4c50c54c6269b2219b69869b33433be447
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-09-25 22:58:46 +0530

    Time: 7 ms (4.44%), Space: 13.3 MB (42.12%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle , int i , int j,vector<vector<int>>&dp){
-        if(i==triangle.size()-1){
-            return triangle[i][j];
-        }
-        if(dp[i][j]!=INT_MAX){
-            return dp[i][j];
-        }
-        int take = 0;
-        take=triangle[i][j]+solve(triangle,i+1,j,dp);
-        int not_take = 0;
-        not_take=triangle[i][j]+solve(triangle,i+1,j+1,dp);
-
-        return dp[i][j]=min(take,not_take);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
-        return solve(triangle , 0 , 0,dp);
+        vector<vector<int>>t = triangle;
+        for(int i = n-2;i>=0;i--){
+            for(int col = 0;col<=i;col++){
+                t[i][col]=t[i][col]+min(t[i+1][col] , t[i+1][col+1]);
+            }
+        }
+        return t[0][0];
     }
 };
