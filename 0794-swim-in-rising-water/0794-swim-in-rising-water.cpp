ad113200ee50a0ed498c240a5718bd4256ff67c8
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-10-06 23:20:40 +0530

    Time: 1 ms (92.89%), Space: 12.8 MB (86.05%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
-vector<vector<int>> directions{{0,1},{1,0},{-1,0},{0,-1}};
+    vector<vector<int>>directions{{0,1},{1,0},{-1,0},{0,-1}};
+    using p = pair<int,pair<int,int>>;
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>>result(n,vector<int>(n,INT_MAX));
+        priority_queue<p,vector<p>,greater<p>>pq;
+        result[0][0]=grid[0][0];
+        pq.push({grid[0][0],{0,0}});
+        while(!pq.empty()){
+            int current = pq.top().first;
+            auto dir = pq.top().second;
+            int fir = dir.first;
+            int sec = dir.second;
+            pq.pop();
 
-bool possible(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>& visited, int mid){
-    int n = grid.size();
-    // out of bounds OR already visited OR cell value > mid → cannot proceed
-    if(i < 0 || i >= n || j < 0 || j >= n || visited[i][j] || grid[i][j] > mid){
-        return false;
-    }
-    // reached destination
-    if(i == n-1 && j == n-1) return true;
+            if(fir==n-1 && sec==n-1){
+                return current;
+            }
+            if(current > result[fir][sec]){
+                continue;
+            }
+            for(auto &dis : directions){
+                int i_=dis[0]+fir;
+                int j_=dis[1]+sec;
 
-    visited[i][j] = true;
-
-    for(auto &dir : directions){
-        int u = i + dir[0];
-        int v = j + dir[1];
-        if(possible(grid, u, v, visited, mid)){
-            return true;
+                if(i_ >= 0 && i_ < n && j_ >= 0 && j_ < n){
+                    int next = max(current,grid[i_][j_]);
+                    if(next<result[i_][j_]){
+                        result[i_][j_]=next;
+                        pq.push({result[i_][j_],{i_,j_}});
+                    }
+                }
+            }
         }
+        return -1;
     }
-    return false;
-}
-
-int swimInWater(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int l = max(grid[0][0], grid[n-1][n-1]);
-    int r = n*n - 1;
-    int result = r;
-
-    while(l <= r){
-        int mid = l + (r - l) / 2;
-        vector<vector<bool>> visited(n, vector<bool>(n, false)); // reset per mid
-
-        if(possible(grid, 0, 0, visited, mid)){
-            result = mid;
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
-    }
-    return result;
-}
-
-
 };
