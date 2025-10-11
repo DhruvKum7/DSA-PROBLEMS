e3f7329053daf7f5cbc2fa9d6805cc289bd6d684
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-10-11 19:11:12 +0530

    Time: 398 ms (36.57%), Space: 230.6 MB (22.71%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -2,19 +2,19 @@ class Solution {
 public: 
     typedef  long long ll;
     vector<ll>t;
-    ll solve(int i , vector<ll>&nums,unordered_map<ll,ll>&mp){
-        if(i>=nums.size()){
-            return 0;
-        }
-        if(t[i]!=-1){
-            return t[i];
-        }
-        int j = lower_bound(nums.begin()+i+1,nums.end(),nums[i]+3)-nums.begin();
-        ll take = nums[i]*mp[nums[i]] + (j<nums.size()?solve(j,nums,mp):0);
-        ll skip = ((i+1)<nums.size()?solve(i+1,nums,mp):0);
+    // ll solve(int i , vector<ll>&nums,unordered_map<ll,ll>&mp){
+    //     if(i>=nums.size()){
+    //         return 0;
+    //     }
+    //     if(t[i]!=-1){
+    //         return t[i];
+    //     }
+    //     int j = lower_bound(nums.begin()+i+1,nums.end(),nums[i]+3)-nums.begin();
+    //     ll take = nums[i]*mp[nums[i]] + (j<nums.size()?solve(j,nums,mp):0);
+    //     ll skip = ((i+1)<nums.size()?solve(i+1,nums,mp):0);
 
-        return t[i]=max(take,skip);
-    }
+    //     return t[i]=max(take,skip);
+    // }
     ll maximumTotalDamage(vector<int>& power) {
         unordered_map<ll,ll>mp;
         
@@ -23,8 +23,17 @@ public:
         vector<ll>nums(mp.size());
         for(auto &i : mp) nums.push_back(i.first);
         sort(nums.begin(),nums.end());
-        t.assign(nums.size()+1,-1);
+        t.assign(nums.size()+1,0);
+        ll result = -1e9;
+        for(int i=nums.size()-1 ;i>=0 ;i--){
+            int j = lower_bound(nums.begin()+i+1,nums.end(),nums[i]+3)-nums.begin();
+            ll take = nums[i]*mp[nums[i]] + (j<nums.size()?t[j]:0);
+            ll skip = i<nums.size()?t[i+1]:0; 
 
-        return solve(0,nums,mp);
+            t[i] = max(take,skip);
+            result = max(t[i],result);
+        }
+
+        return result;
     }
 };
