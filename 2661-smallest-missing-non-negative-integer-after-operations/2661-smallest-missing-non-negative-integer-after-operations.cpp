2c884edf380d1a5c5092a22c31cef9e860371c0c
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-10-16 22:22:26 +0530

    Time: 68 ms (50.1%), Space: 135.9 MB (25.15%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -3,7 +3,7 @@ public:
     int findSmallestInteger(vector<int>& nums, int value) {
         unordered_map<int,int>mp;
         for(auto &i : nums){
-            int ans = ((i%value)+value)%value;
+            int ans = ((i%value)+value)%value;//important line to keep in mind to check modulo of neagtive no.
             mp[ans]++;
         }
         int mex = 0;
