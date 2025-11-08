837c204570e8bc0474b26a8988d57430b256cc5a
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-11-08 17:26:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (17.66%) - LeetHub

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        vector<int>f(32,0);
+        f[0]=1;
+        for(int i=1;i<=30;i++){
+            f[i]=2*f[i-1]+1;
+        }
+        int result=0;
+        int sign = 1;
+        for(int i=30;i>=0;i--){
+            int ith_bit = ((1<<i)&n);
+            if(ith_bit==0){
+                continue;
+            }
+            if(sign>0){
+                result+=f[i];
+            }else{
+                result-=f[i];
+            }
+            sign=sign*(-1);
+        }
+        return result;
+    }
+};
