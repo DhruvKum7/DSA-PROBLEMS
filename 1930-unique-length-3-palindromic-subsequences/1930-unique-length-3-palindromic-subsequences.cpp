d23a4a95e024577a71b2ad311fb5de806e59e395
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-11-21 22:15:38 +0530

    Time: 185 ms (33.26%), Space: 16.1 MB (43.15%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char>st;
+        unordered_set<int>st;
         int n = s.length();
         for(auto &i : s){
             st.insert(i);
         }
-        long long result=0;
+        long long result = 0;
+        vector<pair<int,int>>vec;
         for(auto &j : st){
             int first = -1;
-            int last = -1;
+            int last  = -1;
             for(int i=0;i<n;i++){
                 if(s[i]==j){
                     if(first==-1){
-                        first=i;
+                        first = i;
                     }
-                     last=i;
+                    last = i;
                 }
-               
             }
-
-            unordered_set<char>st_2;
-            for(int k = first+1;k<last;k++){
-                st_2.insert(s[k]);
+            vec.push_back(make_pair(first,last));
+        }
+        
+        for(auto &v : vec){
+            int fir = v.first;
+            int sec = v.second;
+            unordered_set<int>set;
+            for(int i=fir+1;i<sec;i++){
+                set.insert(s[i]);
             }
-            result+=st_2.size();
+            result+=set.size();
         }
         return result;
     }
