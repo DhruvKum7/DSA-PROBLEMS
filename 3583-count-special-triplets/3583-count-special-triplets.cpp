165f20ac8a98f2acaebf2af28a72da2a0a18fe44
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-12-09 21:33:25 +0530

    Time: 731 ms (64.23%), Space: 402.9 MB (55.47%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -4,19 +4,14 @@ public:
     int specialTriplets(vector<int>& nums) {
         unordered_map<int,int>mp1;
         unordered_map<int,int>mp2;
-        for(auto &i : nums){
-            mp2[i]++;
+        long long result = 0;
+        for(auto &num : nums){
+            if(num%2==0){
+                result = (result+mp2[num/2])%m;
+            }
+            mp2[num] = (mp2[num]+(mp1[num*2]))%m;
+            mp1[num]++;
         }
-        long long count = 0;
-    for(int i = 0;i<nums.size();i++){
-        int new_i = 2*nums[i];
-        mp2[nums[i]]--;
-        if((mp1.find(new_i)!=mp1.end()) && (mp2.find(new_i)!=mp2.end())){
-            count=(count+((1LL)*mp1[new_i]*mp2[new_i])%m)%m;
-        }
-        mp1[nums[i]]++;
-    }
-    return count;
-
+        return result;
     }
 };
