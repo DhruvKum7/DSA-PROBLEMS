a0d5a26090c35fa09c6a9e4e7df1e541c2da9f25
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-12-16 12:48:41 +0530

    Time: 19 ms (74.17%), Space: 58.8 MB (21.63%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
+    const int m = 1e9+7;
+
     int numberOfWays(string corridor) {
-        // Store 1000000007 in a variable for convenience
-        const int MOD = 1e9 + 7;
-
-        // Initial values of three variables
-        int zero = 0;
-        int one = 0;
-        int two = 1;
-
-        // Compute using derived equations
-        for (char thing : corridor) {
-            if (thing == 'S') {
-                zero = one;
-                swap(one, two);
-            } else {
-                two = (two + zero) % MOD;
-            }
+        vector<int> seats;
+        for (int i = 0; i < (int)corridor.size(); i++) {
+            if (corridor[i] == 'S') seats.push_back(i);
         }
 
-        // Return the result
-        return zero;
+        if (seats.empty() || (seats.size() & 1)) return 0;
+
+        long long ans = 1;
+        int prevSecond = seats[1];
+        for (int i = 2; i < (int)seats.size(); i += 2) {
+            ans = (ans * (seats[i] - prevSecond)) % m;
+            prevSecond = seats[i + 1];
+        }
+        return (int)ans;
     }
 };
