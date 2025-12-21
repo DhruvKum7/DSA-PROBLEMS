05a05623b712ac74573a19e1bd24a069f5f08efd
Dhruv Kumar <158982893+DhruvKum7@users.noreply.github.com>
2025-12-21 18:07:34 +0530

    Time: 0 ms (100%), Space: 12.8 MB (62.92%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int rows = strs.size(); //Number of rows
-        int cols = strs[0].size(); //Number of columns
+        int  n = strs.size();
+        int  m = strs[0].size();
 
-        int deletion = 0;
-
-        vector<bool> alreadySorted(rows, false);
-
-        int row = 0;
-        int col = 0;
-        //to iterate in the strs
-        
-        for(col = 0; col < cols; col++) {
-            bool deleted = false;
-
-            for(row = 0; row < rows-1; row++) {
-                if(!alreadySorted[row] && strs[row][col] > strs[row+1][col]) {
-                    deletion++;
-                    deleted = true;
+        int deleted = 0;
+        bool count;
+        vector<bool>counted(n-1,false);
+        for(int i=0;i<m;i++){
+            count = false;
+            for(int j=0;j<n-1;j++){
+                if(counted[j]==false && (strs[j][i]>strs[j+1][i])){
+                   count=true;
                     break;
                 }
             }
-
-            if(deleted) {
+            if(count==true){
+                 deleted++;
                 continue;
+            }else{
+                for(int k=0;k<n-1;k++){
+                   
+                    if(!counted[k] && strs[k][i]<strs[k+1][i]){
+                        counted[k]=true;
+                    }
+                }
+                
             }
-
-            for(int i = 0; i < rows-1; i++) {
-                alreadySorted[i] = alreadySorted[i] | (strs[i][col] < strs[i+1][col]);
-            }
-
         }
-
-        return deletion;
+        return deleted;
     }
 };
